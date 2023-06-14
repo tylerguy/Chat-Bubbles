5a4ee9fe83d84db4274e9d176aaf749f956672d8
tylerguy <tylerdenman200@gmail.com>
2023-06-14 01:30:53 +0100

    remvoe username input form, redundant

--- src/App.js
+++ src/App.js
@@ -1,13 +1,11 @@
 import React from "react";
 import { HashRouter as Router, Route, Routes } from "react-router-dom";
 import Chat from "./Chat";
-import UsernameForm from "./UsernameForm";
 
 function App() {
   return (
     <Router>
       <Routes>
-        <Route path="/" element={<UsernameForm />} />
         <Route path="/chat/:username" element={<Chat />} />
       </Routes>
     </Router>
